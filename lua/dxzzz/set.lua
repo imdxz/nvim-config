6d42822a7ab3328efa7537f82bc6a340fd54dcbb
dxZhang <dxzhang49@outlook.com>
2026-03-18 11:16:51 +0800

    Fix clipboard over ssh

--- lua/dxzzz/set.lua
+++ lua/dxzzz/set.lua
@@ -35,3 +35,15 @@ vim.opt.colorcolumn = "80"
 
 vim.g.netrw_altv = 1
 
+-- Use osc52 for clipboard
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
